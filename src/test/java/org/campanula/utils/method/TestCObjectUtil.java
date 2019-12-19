419dbb824c21cbcd576cee9fbfe3cc4baf4a8f35
liweiyi <liweiyi@91jinrong.com>
2019-12-19 16:36:48 +0800

    feat: process ut

--- src/test/java/org/campanula/utils/method/TestCObjectUtil.java
+++ src/test/java/org/campanula/utils/method/TestCObjectUtil.java
@@ -41,4 +41,53 @@ public class TestCObjectUtil {
         String s1 = CObjectUtil.get(() -> null, () -> s);
         Assert.assertEquals(s, s1);
     }
+
+    @Test
+    public void nullRuntimeThrows() {
+        thrown.expect(MethodNullException.class);
+        CObjectUtil.nullRuntimeThrows(null, RuntimeException::new);
+    }
+
+    @Test
+    public void nullRuntimeThrows1() {
+        thrown.expect(RuntimeException.class);
+        CObjectUtil.nullRuntimeThrows(() -> null, RuntimeException::new);
+    }
+
+    @Test
+    public void nullRuntimeThrows2() {
+        String s = "1";
+        String s1 = CObjectUtil.nullRuntimeThrows(() -> s, RuntimeException::new);
+        Assert.assertEquals(s, s1);
+    }
+
+    @Test
+    public void nullRuntimeThrows3() {
+        thrown.expect(NullPointerException.class);
+        CObjectUtil.nullRuntimeThrows(() -> null, () -> null);
+    }
+
+    @Test
+    public void process() {
+        thrown.expect(MethodNullException.class);
+        CObjectUtil.process(Object::new, null, Object::new);
+    }
+
+    @Test
+    public void process1() {
+        String process = CObjectUtil.process(() -> "1", (s) -> s.concat("2"), null);
+        Assert.assertEquals(process, "12");
+    }
+
+    @Test
+    public void process2() {
+        thrown.expect(MethodNullException.class);
+        String process = CObjectUtil.process(() -> null, (s) -> s.concat("2"), null);
+    }
+
+    @Test
+    public void process3() {
+        String process = CObjectUtil.process(() -> null, (s) -> s.concat("2"), () -> "1");
+        Assert.assertEquals(process, "1");
+    }
 }
